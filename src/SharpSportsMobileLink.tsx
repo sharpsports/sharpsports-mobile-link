328255643dedf1d957e30e44cf0661783c2afef3
Vahagn Martirosyan <vahagnmartirosyan@Vahagns-Mac-mini.local>
2021-06-24 15:38:26 +0400

    Disable Eslint

--- src/SharpSportsMobileLink.tsx
+++ src/SharpSportsMobileLink.tsx
@@ -3,24 +3,24 @@ import { View, Text, TouchableOpacity, ViewStyle, TextStyle } from 'react-native
 import { WebView, WebViewNavigation } from 'react-native-webview';
 
 export interface Props {
-  internalId: string
-  token: string
-  buttonText: string
-  logoUrl: string
-  paddingVertical: number
-  paddingHorizontal: number
-  backgroundColor: string
-  buttonColor: string
-  borderRadius: number
-  fontFamily: string
-  fontSize: number
+  internalId: string;
+  token: string;
+  buttonText: string;
+  logoUrl: string;
+  paddingVertical: number;
+  paddingHorizontal: number;
+  backgroundColor: string;
+  buttonColor: string;
+  borderRadius: number;
+  fontFamily: string;
+  fontSize: number;
   textAlign: 'center' | 'left' | 'right | justify'
-  onLoading?: () => void
-  onLoadingDismiss?: () => void
-  onError?: () => void
-  presentWebView: (webView: JSX.Element) => void
-  dismissWebView: () => void
-  buttonStyle: ViewStyle
+  onLoading?: () => void;
+  onLoadingDismiss?: () => void;
+  onError?: () => void;
+  presentWebView: (webView: JSX.Element) => void;
+  dismissWebView: () => void;
+  buttonStyle: ViewStyle;
   buttonTextStyle: TextStyle
 }
 
@@ -50,7 +50,6 @@ class SharpSportsMobileLink extends React.Component<Props> {
       buttonStyle,
     ]
 
-
     const buttonTextStyles = [
       {
         color: buttonColor,
@@ -71,58 +70,50 @@ class SharpSportsMobileLink extends React.Component<Props> {
     )
   }
 }
+
 const buildURL = (data: any, logoUrl: string) => {
-  return logoUrl
-    ? `https://ui.sharpsports.io/link/${data.cid}?user_logo=${logoUrl}`
-    : `https://ui.sharpsports.io/link/${data.cid}`
+  return logoUrl ? `https://ui.sharpsports.io/link/${data.cid}?user_logo=${logoUrl}` : `https://ui.sharpsports.io/link/${data.cid}`;
 }
 
 const fetchIntegration = (props: Props) => {
-  const { internalId, token, logoUrl } = props
-  props.onLoading?.()
-  postContext(
-    'https://api.sharpsports.io/v1/context',
-    { internalId: internalId },
-    token
-  )
-    .then((data) => {
-      props.onLoadingDismiss?.()
+  const { internalId, token, logoUrl } = props;
+  props.onLoading?.();
+  postContext('https://api.sharpsports.io/v1/context', {internalId: internalId}, token)
+    .then(data => {
+      props.onLoadingDismiss?.();
       props.presentWebView(
         <WebView
-          source={{ uri: buildURL(data, logoUrl) }}
-          style={{ justifyContent: 'center' }}
-          onNavigationStateChange={(newNavState: WebViewNavigation) =>
+          source={{uri: buildURL(data, logoUrl)}}
+          style={{justifyContent: "center"}}
+          onNavigationStateChange={ (newNavState: WebViewNavigation) =>
             handleWebViewNavigationStateChange(props, newNavState)
           }
         />
       )
     })
-    .catch((error) => {
+    .catch(error => {
       console.log(`Error occurred: ${error}`)
       props.onError?.()
     })
 }
 
-const postContext = async (url: string, data = {}, token: string) => {
+const postContext = async(url: string, data = {}, token: string) => {
   const response = await fetch(url, {
     method: 'POST',
     headers: {
-      Authorization: `Token ${token}`,
-      'Content-Type': 'application/json',
+      'Authorization': `Token ${token}`,
+      'Content-Type': 'application/json'
     },
-    body: JSON.stringify(data),
-  })
-  return response.json()
+    body: JSON.stringify(data)
+  });
+  return response.json();
 }
 
-const handleWebViewNavigationStateChange = (
-  props: Props,
-  newNavState: WebViewNavigation
-) => {
-  const { url } = newNavState
+const handleWebViewNavigationStateChange = (props: Props, newNavState: WebViewNavigation) => {
+  const { url } = newNavState;
   if (url.includes('/done')) {
-    props.dismissWebView()
+    props.dismissWebView();
   }
 }
 
-export default SharpSportsMobileLink
+export default SharpSportsMobileLink;
