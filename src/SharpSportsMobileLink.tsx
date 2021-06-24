c47ecfb561a778dc9e66081629bf15dd4df26ec0
Vahagn Martirosyan <vahagnmartirosyan@Vahagns-Mac-mini.local>
2021-06-24 16:44:23 +0400

    Rollback code

--- src/SharpSportsMobileLink.tsx
+++ src/SharpSportsMobileLink.tsx
@@ -1,33 +1,25 @@
-import * as React from 'react'
-import {
-  Text,
-  TextStyle,
-  TouchableOpacity,
-  View,
-  ViewStyle,
-} from 'react-native'
-import { WebView, WebViewNavigation } from 'react-native-webview'
+import * as React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { WebView, WebViewNavigation } from 'react-native-webview';
 
 export interface Props {
-  internalId: string
-  token: string
-  buttonText: string
-  logoUrl: string
-  paddingVertical: number
-  paddingHorizontal: number
-  backgroundColor: string
-  buttonColor: string
-  borderRadius: number
-  fontFamily: string
-  fontSize: number
+  internalId: string;
+  token: string;
+  buttonText: string;
+  logoUrl: string;
+  paddingVertical: number;
+  paddingHorizontal: number;
+  backgroundColor: string;
+  buttonColor: string;
+  borderRadius: number;
+  fontFamily: string;
+  fontSize: number;
   textAlign: 'center' | 'left' | 'right | justify'
-  onLoading?: () => void
-  onLoadingDismiss?: () => void
-  onError?: () => void
-  presentWebView: (webView: JSX.Element) => void
-  dismissWebView: () => void
-  buttonStyle: ViewStyle
-  buttonTextStyle: TextStyle
+  onLoading?: () => void;
+  onLoadingDismiss?: () => void;
+  onError?: () => void;
+  presentWebView: (webView: JSX.Element) => void;
+  dismissWebView: () => void;
 }
 
 class SharpSportsMobileLink extends React.Component<Props> {
@@ -39,96 +31,79 @@ class SharpSportsMobileLink extends React.Component<Props> {
       borderRadius,
       textAlign,
       buttonColor,
-      buttonStyle,
       fontSize,
-      fontFamily,
-    } = this.props
+      fontFamily
+    } = this.props;
 
-    const buttonStyles = [
-      {
-        backgroundColor,
-        paddingVertical,
-        paddingHorizontal,
-        borderRadius,
-        textAlign,
-      },
-      buttonStyle,
-    ]
+    const buttonStyle = {
+      backgroundColor,
+      paddingVertical,
+      paddingHorizontal,
+      borderRadius,
+      textAlign
+    }
 
-    const buttonTextStyles = [
-      {
-        color: buttonColor,
-        fontSize,
-        fontFamily,
-      },
-      buttonTextStyle,
-    ]
+    const buttonTextStyle = {
+      color: buttonColor,
+      fontSize,
+      fontFamily
+    }
 
     return (
-      <TouchableOpacity
-        onPress={() => fetchIntegration(this.props)}
-        style={{ justifyContent: 'center' }}
-      >
-        <View style={buttonStyles}>
-          <Text style={buttonTextStyles}>{this.props.buttonText}</Text>
-        </View>
-      </TouchableOpacity>
+        <TouchableOpacity onPress={() => fetchIntegration(this.props)} style={{justifyContent: "center" }}>
+          <View style={{ ...buttonStyle }}>
+            <Text style={{ ...buttonTextStyle }}>
+              { this.props.buttonText }
+            </Text>
+          </View>
+        </TouchableOpacity>
     )
   }
 }
 
 const buildURL = (data: any, logoUrl: string) => {
-  return logoUrl
-    ? `https://ui.sharpsports.io/link/${data.cid}?user_logo=${logoUrl}`
-    : `https://ui.sharpsports.io/link/${data.cid}`
+  return logoUrl ? `https://ui.sharpsports.io/link/${data.cid}?user_logo=${logoUrl}` : `https://ui.sharpsports.io/link/${data.cid}`;
 }
 
 const fetchIntegration = (props: Props) => {
-  const { internalId, token, logoUrl } = props
-  props.onLoading?.()
-  postContext(
-    'https://api.sharpsports.io/v1/context',
-    { internalId: internalId },
-    token
-  )
-    .then((data) => {
-      props.onLoadingDismiss?.()
-      props.presentWebView(
-        <WebView
-          source={{ uri: buildURL(data, logoUrl) }}
-          style={{ justifyContent: 'center' }}
-          onNavigationStateChange={(newNavState: WebViewNavigation) =>
-            handleWebViewNavigationStateChange(props, newNavState)
-          }
-        />
-      )
-    })
-    .catch((error) => {
-      console.log(`Error occurred: ${error}`)
-      props.onError?.()
-    })
+  const { internalId, token, logoUrl } = props;
+  props.onLoading?.();
+  postContext('https://api.sharpsports.io/v1/context', {internalId: internalId}, token)
+      .then(data => {
+        props.onLoadingDismiss?.();
+        props.presentWebView(
+            <WebView
+                source={{uri: buildURL(data, logoUrl)}}
+                style={{justifyContent: "center"}}
+                onNavigationStateChange={ (newNavState: WebViewNavigation) =>
+                    handleWebViewNavigationStateChange(props, newNavState)
+                }
+            />
+        )
+      })
+      .catch(error => {
+        console.log(`Error occurred: ${error}`)
+        props.onError?.()
+      })
 }
 
-const postContext = async (url: string, data = {}, token: string) => {
+const postContext = async(url: string, data = {}, token: string) => {
   const response = await fetch(url, {
     method: 'POST',
     headers: {
-      Authorization: `Token ${token}`,
-      'Content-Type': 'application/json',
+      'Authorization': `Token ${token}`,
+      'Content-Type': 'application/json'
     },
-    body: JSON.stringify(data),
-  })
-  return response.json()
+    body: JSON.stringify(data)
+  });
+  return response.json();
 }
 
-const handleWebViewNavigationStateChange = (
-  props: Props,
-  newNavState: WebViewNavigation
-) => {
-  const { url } = newNavState
+const handleWebViewNavigationStateChange = (props: Props, newNavState: WebViewNavigation) => {
+  const { url } = newNavState;
   if (url.includes('/done')) {
-    props.dismissWebView()
+    props.dismissWebView();
   }
 }
 
-export default SharpSportsMobileLink
+export default SharpSportsMobileLink;
